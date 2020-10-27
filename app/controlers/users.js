17e9c06a1ac1405ff0f76990ff032434edcc1ed8
TankLu <85423262@qq.com>
2020-10-28 00:33:49 +0800

    chapter6

--- app/controlers/users.js
+++ app/controlers/users.js
@@ -2,20 +2,38 @@ const db = [{name: '李雷'}];
 
 class UsersCtl {
     find(ctx) {
+        a.b
         ctx.body = db;
     }
     findById(ctx) {
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412, '先决条件失败');
+        }
         ctx.body = db[ctx.params.id * 1];
     }
     create(ctx) {
+        ctx.verifyParams({
+            name: {type: 'string', required: true},
+            age: {type: 'number', required: false}
+        });
         db.push(ctx.request.body);
         ctx.body = ctx.request.body;
     }
     update(ctx) {
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412, '先决条件失败');
+        }
+        ctx.verifyParams({
+            name: {type: 'string', required: true},
+            age: {type: 'number', required: false}
+        });
         db[ctx.params.id * 1] = ctx.request.body;
         ctx.body = ctx.request.body;
     }
     delete(ctx) {
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412, '先决条件失败');
+        }
         db.splice(ctx.params.id * 1, 1);
         ctx.status = 204;
     }
--- app/index.js
+++ app/index.js
@@ -1,9 +1,27 @@
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
+const error = require('koa-json-error');
+const parameter = require('koa-parameter');
 const app = new Koa();
 const routing = require('./routes');
-
+app.use(async (ctx, next) => {
+  try{
+    await next();
+  }catch(err) {
+    ctx.status = err.status || err.statusCode || 500;
+    ctx.body = {
+      message: err.message
+    }
+  }
+});
+app.use(error({
+  postFormat: (err, {stack, ...rest}) => {
+    return process.env.NODE_ENV === 'production' ?
+      rest: {stack, ...rest};
+  }
+}));
 app.use(bodyparser());
+app.use(parameter(app));
 routing(app);
 
 app.listen(3000, () => {

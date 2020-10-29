092eb53767dedb0ecd9f65a4bc26c77e418db379
TankLu <85423262@qq.com>
2020-10-30 00:19:11 +0800

    'chapter8'

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,11 +1,19 @@
 const Router = require('koa-router');
 const router = new Router({prefix: '/users'});
-const {find, findById, create, update, delete:del} = require('../controlers/users');
+const {find, findById, create, update, delete:del,
+    login} = require('../controlers/users');
+
+const auth = async (ctx) => {
+
+};
 
 router.get('/', find);
 router.post('/', create);
 router.get('/:id', findById);
-router.put('/:id', update);
+// router.put('/:id', update); // 整体更新
+router.patch('/:id', update); // 部分更新
 router.delete('/:id', del);
+
+router.post('/login', login);
 
 module.exports = router;

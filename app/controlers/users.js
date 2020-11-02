8cdba0f19920c9f7d31371fc9db8e0e38a47c6bb
Tank <85423262@qq.com>
2020-11-02 13:41:00 +0800

    chapter10

--- app/controlers/users.js
+++ app/controlers/users.js
@@ -7,7 +7,9 @@ class UsersCtl {
         ctx.body = await User.find();
     }
     async findById(ctx) {
-        const user = await User.findById(ctx.params.id);
+         const { fields } = ctx.query;
+         const selectFields = fields.split(';').filter((f) => f).map((f) => ' +' + f).join('');
+        const user = await User.findById(ctx.params.id).select(selectFields);
         if (!user) {
             ctx.throw(404, '用户不存在');
         }
@@ -27,11 +29,18 @@ class UsersCtl {
 
         const user = await new User(ctx.request.body).save();
         ctx.body = await User.findById(user._id);
-    } 
+    }
     async update(ctx) {
         ctx.verifyParams({
             name: {type: 'string', required: false},
-            password: {type: 'string', required: false}
+            password: {type: 'string', required: false},
+            avatar_url: {type: 'string', required: false},
+            gender: {type: 'string', required: false},
+            headline: {type: 'string', required: false},
+            locations: {type: 'array', itemType: 'string', required: false},
+            business: {type: 'string', required: false},
+            employments: {type: 'array', itemType:'object', required: false},
+            educations: {type: 'array', itemType:'object', required: false}
         });
         const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body);
         if(!user) {
